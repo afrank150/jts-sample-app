ef622f8a5afac6b146cd5035da69139822819146
afrank <arfrank3@gmail.com>
2020-10-26 21:42:23 -0600

    inital project setup

---
+++ src/main/java/com/skander/jts/sample/app/JtsSampleAppApplication.java
@@ -0,0 +1,13 @@
+package com.skander.jts.sample.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JtsSampleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JtsSampleAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skander/jts/sample/app/JtsSampleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skander.jts.sample.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JtsSampleAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
